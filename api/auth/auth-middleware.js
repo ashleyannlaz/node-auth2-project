e2491e5777f31730fa459ebf6636b0f4aa743ac5
ashleyannlaz <paperpastels@gmail.com>
2021-09-14 22:19:22 -0400

    MVP, needs cleaning.

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,9 +1,10 @@
 const { JWT_SECRET,  } = require("../secrets"); // use this secret!
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const {findBy} = require('../users/users-model');
 
-const restricted = (req, res, next) => {
+const restricted = async (req, res, next) => {
 
-  const token = req.header.authorization
+  const token = req.headers.authorization
   if(token) {
     jwt.verify(token, JWT_SECRET, (error, decoded) => {
       if(error) {
@@ -16,9 +17,6 @@ const restricted = (req, res, next) => {
   } else {
     next({status: 401, message:'Token required'})
   }
-
-
-
 
   /*
     If the user does not provide a token in the Authorization header:
@@ -57,7 +55,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -65,7 +63,18 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-    next()
+ try {
+const[user] = await findBy({username: req.body.username})
+if(!user) {
+  next({status: 401, message:'Invalid credentials'})
+} else {
+  req.user = user
+  next()
+}
+ } catch(error) {
+    next(error)
+ }
+    
 }
 
 
@@ -88,7 +97,18 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-    next()
+    const {role_name} = req.body
+    if(!role_name || !role_name.trim()) {
+      req.role_name = 'student'
+      next()
+    } else if (role_name.trim() === 'admin') {
+      next({status: 422, message: 'Role name can not be admin'})
+    } else if (role_name.trim().length > 32) {
+      next({status: 422, message:'Role name can not be longer than 32 chars'})
+    } else {
+      req.role_name = role_name.trim()
+      next()
+    }
 }
 
 module.exports = {
